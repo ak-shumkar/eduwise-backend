c21a0e2f8af1799f2cb232f8f9bdd44dc7a742fa
mamatkasym <mm.kalandar@gmail.com>
2021-07-23 22:00:16 +0600

    add abstract app

---
+++ abstract/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+
+class AbstractDateModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class AbstractActiveModel(models.Model):
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class AbstractLocaleModel(models.Model):
+    locale = models.CharField(max_length=2, default='en')
+
+    class Meta:
+        abstract = True
+
+
+class AbstractDateLocaleModel(AbstractDateModel, AbstractLocaleModel):
+    pass
+
+
+class AbstractModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
