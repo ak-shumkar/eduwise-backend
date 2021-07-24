60807a620db39de3137f071802d8d5003e01a5fe
mamatkasym <mm.kalandar@gmail.com>
2021-07-24 13:22:32 +0600

    change auth package to djoser

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', include('dj_rest_auth.urls')),
-    path('register/', include('dj_rest_auth.registration.urls')),
-    path('facebook/', views.FacebookLogin.as_view(), name='fb_login')
+    # path('', include('dj_rest_auth.urls')),
+    # path('register/', include('dj_rest_auth.registration.urls')),
+    path('', include('djoser.urls')),
+    path('', include('djoser.urls.jwt')),
+    path('facebook/', views.FacebookLogin.as_view(), name='fb_login'),
+    path('google/', views.GoogleLogin.as_view(), name='google_login')
 ]
