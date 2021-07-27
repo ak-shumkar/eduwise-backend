292b18347a045fe0433ab5cf5fe6cedd7a54204c
mamatkasym <mm.kalandar@gmail.com>
2021-07-27 21:45:29 +0600

    improve institution crud api

--- institutions/serializers.py
+++ institutions/serializers.py
@@ -3,17 +3,17 @@ from . import models
 
 
 class InstitutionI18NSerializer(AbstractModelSerializer):
-    class Meta:
+    class Meta(AbstractModelSerializer.Meta):
         model = models.InstitutionI18N
 
 
 class InstitutionSerializer(AbstractModelSerializer):
     translations = InstitutionI18NSerializer(many=True, read_only=True)
 
-    class Meta:
+    class Meta(AbstractModelSerializer.Meta):
         model = models.Institution
 
 
 class PhotoSerializer(AbstractModelSerializer):
-    class Meta:
+    class Meta(AbstractModelSerializer.Meta):
         model = models.Photo
--- institutions/views.py
+++ institutions/views.py
@@ -1,22 +1,27 @@
-from django.shortcuts import render
 from abstract.views import AbstractViewSet
 from utils.permissions import IsAdministrator
 from . import models, serializers
 
 
 class InstitutionViewSet(AbstractViewSet):
+    """ CREATE, GET, UPDATE, DELETE universities """
+    model = models.Institution
     queryset = models.Institution.objects.all()
     serializer_class = serializers.InstitutionSerializer
     permission_classes = [IsAdministrator]
 
 
 class InstitutionI18NViewSet(AbstractViewSet):
+    """ CREATE, GET, UPDATE, DELETE universities translations """
+    model = models.InstitutionI18N
     queryset = models.InstitutionI18N.objects.all()
     serializer_class = serializers.InstitutionI18NSerializer
     permission_classes = [IsAdministrator]
 
 
 class PhotoViewSet(AbstractViewSet):
+    """ CREATE, GET, UPDATE, DELETE universities gallery """
+    model = models.Photo
     queryset = models.Photo.objects.all()
     serializer_class = serializers.PhotoSerializer
     permission_classes = [IsAdministrator]
