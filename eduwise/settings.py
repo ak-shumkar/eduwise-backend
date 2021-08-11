b8c64c288854008917a27dcc01af3a3b086c0be8
mamatkasym <mm.kalandar@gmail.com>
2021-08-11 11:41:32 +0600

    add 128.199.86.102 to allowed hosts

--- eduwise/settings.py
+++ eduwise/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-$xtn-y&n(gj6+byfb9!*s8m_lx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '128.199.86.102']
 CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
