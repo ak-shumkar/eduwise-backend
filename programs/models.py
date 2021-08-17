7c1b80ebd5e288682a7d7f4a510ddd589669b1ec
mamatkasym <mm.kalandar@gmail.com>
2021-08-17 23:46:19 +0600

    created programs app

--- programs/models.py
+++ programs/models.py
@@ -3,26 +3,38 @@ from django.db import models
 
 
 class ProgramType(AbstractDateModel):
+    """ Types like Bachelor's, Master's, PHD """
     name = models.CharField(max_length=128)
 
 
 class ProgramTypeI18N(AbstractDateLocaleModel):
+    """ Program Type translation"""
     name = models.CharField(max_length=128)
 
 
 class Term(AbstractDateModel):
+    """ Program start season like Winter 2022, Spring 2023"""
     season = models.CharField(max_length=32)
     year = models.IntegerField()
 
 
 class TermI18N(AbstractDateLocaleModel):
+    """ Term translation """
     season = models.CharField(max_length=32)
     term = models.ForeignKey(Term, related_name='translations', on_delete=models.PROTECT)
 
 
 class Program(AbstractDateModel):
+    """ Program """
     overview = models.TextField()
     title = models.CharField(max_length=128)
     link = models.URLField(null=True, blank=True)
     institution = models.ForeignKey('institutions.Institution', on_delete=models.PROTECT, related_name='programs')
     term = models.ForeignKey(Term, on_delete=models.PROTECT, related_name='programs')
+
+
+class ProgramI18N(AbstractDateLocaleModel):
+    """ Program translation """
+    program = models.ForeignKey(Program, on_delete=models.PROTECT, related_name='translations')
+    overview = models.TextField()
+    title = models.CharField(max_length=128)
---
+++ programs/serializers.py
@@ -0,0 +1,41 @@
+from . import models
+from abstract.serializers import AbstractModelSerializer
+
+
+class ProgramTypeI18NSerializer(AbstractModelSerializer):
+
+    class Meta(AbstractModelSerializer.Meta):
+        model = models.ProgramTypeI18N
+
+
+class ProgramTypeSerializer(AbstractModelSerializer):
+    translations = ProgramTypeI18NSerializer(many=True, read_only=True)
+
+    class Meta(AbstractModelSerializer.Meta):
+        model = models.ProgramType
+        depth = 1
+
+
+class TermI18NSerializer(AbstractModelSerializer):
+    class Meta(AbstractModelSerializer.Meta):
+        model = models.TermI18N
+
+
+class TermSerializer(AbstractModelSerializer):
+    translations = TermI18NSerializer(many=True, read_only=True)
+
+    class Meta(AbstractModelSerializer.Meta):
+        model = models.Term
+        depth = 1
+
+
+class ProgramI18NSerializer(AbstractModelSerializer):
+    class Meta(AbstractModelSerializer.Meta):
+        model = models.ProgramTypeI18N
+
+
+class ProgramSerializer(AbstractModelSerializer):
+    translations = ProgramI18NSerializer(many=True, read_only=True)
+
+    class Meta(AbstractModelSerializer.Meta):
+        model = models.Program
