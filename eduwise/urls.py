1360b6db2a86d195804b5894eda0640b7288dc3f
mamatkasym <mm.kalandar@gmail.com>
2021-09-03 21:39:17 +0600

    made all backend urls with api

--- eduwise/urls.py
+++ eduwise/urls.py
@@ -28,8 +28,8 @@ urlpatterns = [
     path('api/', include('institutions.urls')),
     path('api/', include('programs.urls')),
     path('api/auth/', include('users.urls')),
-    path('oddmin/', admin.site.urls),  # Do not use in production
-    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/oddmin/', admin.site.urls),  # Do not use in production
+    url(r'^api/swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^api/swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^api/redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
