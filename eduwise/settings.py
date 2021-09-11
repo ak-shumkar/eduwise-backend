9db7402bf8b0f0d7b3278b9fd78352c4b43d19d2
mamatkasym <mm.kalandar@gmail.com>
2021-09-11 12:33:47 +0600

    change static_root and media_root

--- eduwise/settings.py
+++ eduwise/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'api/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -210,7 +210,7 @@ SIMPLE_JWT = {
 }
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'api/media')
 
 # REST_AUTH_TOKEN_MODEL = 'rest_framework_simplejwt.tokens.Token'
 SWAGGER_SETTINGS = {
