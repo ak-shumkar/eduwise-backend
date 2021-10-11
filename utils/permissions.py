e1f95c922131750890acb193b35c122813cf51de
mamatkasym <mm.kalandar@gmail.com>
2021-10-11 18:26:22 +0600

    added permission for students and agents

--- utils/permissions.py
+++ utils/permissions.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class IsAdministrator(BasePermission):
@@ -9,3 +9,21 @@ class IsAdministrator(BasePermission):
         if user.is_anonymous:
             return False
         return bool(user.is_superuser)
+
+
+class IsStudentOrAgent(BasePermission):
+    message = 'Only students and agents can apply'
+
+    def has_permission(self, request, view):
+        user = request.user
+        if request.method in SAFE_METHODS:
+            return bool(user.is_authenticated)
+        return bool(user.is_authenticated and (user.is_student or user.is_agent))
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        if hasattr(obj, 'owner'):
+            return obj.owner == request.user
+        if hasattr(obj, 'user'):
+            return obj.user == request.user
