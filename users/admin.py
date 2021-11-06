133c5a9020fc4c2d1866bf84622aef7e9d36084c
mamatkasym <mm.kalandar@gmail.com>
2021-11-06 17:38:05 +0600

    hide user info from modifying

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import User
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'role', 'date_joined')
+    readonly_fields = ('password', 'last_login', 'is_superuser', 'username', 'first_name', 'last_name', 'email', 'is_staff', 'is_active', 'date_joined', 'role', 'groups', 'user_permissions')
+
+    class Meta:
+        model = User
+
+
+admin.site.register(User, UserAdmin)
