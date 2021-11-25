5c5cfee4f3f1f28cde0e94958c52289d9433059c
mamatkasym <mm.kalandar@gmail.com>
2021-11-25 16:04:03 +0600

    added model tests

--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -5,9 +5,36 @@ User = get_user_model()
 
 
 @pytest.fixture
-def test_student():
-    student = User(username='alipay', password='secret')
-    print(student)
-    print(student.__class__.__mro__)
+def student():
+    student = User(username='student', password='secret')
     return student
 
+
+@pytest.fixture
+def agent():
+    agent = User(username='agent', password='secret', role='A')
+    return agent
+
+
+@pytest.mark.django_db
+def test_create_default_user():
+    user = User(username="alex", password='secret')
+    user.full_clean()
+    user.save()
+    db_user = User.objects.all()[0]
+    assert db_user is not None
+    assert db_user.is_student
+    assert not db_user.is_agent
+    assert not db_user.is_staff
+
+    # default fields
+    assert db_user.date_joined is not None
+    assert db_user.is_active
+
+
+def test_is_student(student):
+    assert student.is_student
+
+
+def test_is_agent(agent):
+    assert agent.is_agent
