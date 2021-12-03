52a74c766758f6fa2bedaa054f671f0aaea11f5b
mamatkasym <mm.kalandar@gmail.com>
2021-12-03 16:34:12 +0600

    added security conf

--- eduwise/settings.py
+++ eduwise/settings.py
@@ -232,3 +232,14 @@ DJOSER = {
 }
 
 ADMINS = [('Mamatkasym', 'mm.kalandar@gmail.com')]
+
+if os.getenv('ENVIRONMENT', 'production') == 'production':
+    SECURE_BROWSER_XSS_FILTER = True
+    X_FRAME_OPTIONS = 'DENY'
+    SECURE_SSL_REDIRECT = True
+    SECURE_HSTS_SECONDS = 3600
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
