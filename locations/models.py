ec2f53e4e8b75677bfd3f47942ac82594a857d3d
mamatkasym <mm.kalandar@gmail.com>
2021-12-04 17:39:57 +0600

    added verbose names

--- locations/models.py
+++ locations/models.py
@@ -9,15 +9,23 @@ class Country(AbstractModel):
 
     class Meta:
         db_table = 'country'
+        verbose_name_plural = 'countries'
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class CountryI18N(AbstractDateLocaleModel):
-    country = models.ForeignKey(Country, related_name='translations', on_delete=models.PROTECT)
-    name = models.CharField(max_length=64)
+    country = models.ForeignKey(Country,
+                                related_name='translations',
+                                on_delete=models.PROTECT,
+                                verbose_name='Original name')
+    name = models.CharField(max_length=64, verbose_name='Translation')
 
     class Meta:
         db_table = 'country_i18n'
         unique_together = ['country', 'locale']
+        verbose_name = 'country translation'
 
 
 class Province(AbstractModel):
@@ -28,14 +36,21 @@ class Province(AbstractModel):
         db_table = 'province'
         unique_together = ['name', 'country']
 
+    def __str__(self):
+        return f"{self.name}, {self.country.name}"
+
 
 class ProvinceI18N(AbstractDateLocaleModel):
-    province = models.ForeignKey(Province, related_name='translations', on_delete=models.PROTECT)
-    name = models.CharField(max_length=64)
+    province = models.ForeignKey(Province,
+                                 related_name='translations',
+                                 on_delete=models.PROTECT,
+                                 verbose_name='Original name')
+    name = models.CharField(max_length=64, verbose_name='Translation')
 
     class Meta:
         db_table = 'province_i18n'
         unique_together = ['province', 'locale']
+        verbose_name = 'province translation'
 
 
 class City(AbstractModel):
@@ -45,12 +60,20 @@ class City(AbstractModel):
     class Meta:
         db_table = 'city'
         unique_together = ['name', 'province']
+        verbose_name_plural = 'cities'
+
+    def __str__(self):
+        return f"{self.name}, {self.province.name}, {self.province.country.name}"
 
 
 class CityI18N(AbstractDateLocaleModel):
-    city = models.ForeignKey(City, related_name='translations', on_delete=models.PROTECT)
-    name = models.CharField(max_length=64)
+    city = models.ForeignKey(City,
+                             related_name='translations',
+                             on_delete=models.PROTECT,
+                             verbose_name='Original name')
+    name = models.CharField(max_length=64, verbose_name='Translation')
 
     class Meta:
         db_table = 'city_i18n'
         unique_together = ['city', 'locale']
+        verbose_name = 'city translation'
