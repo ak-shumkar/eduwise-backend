ae073239afb4affe9deaec069486626b24de6c4a
mamatkasym <mm.kalandar@gmail.com>
2021-12-08 00:36:26 +0600

    improve fb login view

--- users/views.py
+++ users/views.py
@@ -6,6 +6,7 @@ from dj_rest_auth.registration.views import SocialLoginView
 
 class FacebookLogin(SocialLoginView):
     adapter_class = FacebookOAuth2Adapter
+    client_class = OAuth2Client
 
 
 class GoogleLogin(SocialLoginView):
