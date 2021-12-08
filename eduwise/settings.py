d19ec9726444d85874a799c852bde4482a177515
mamatkasym <mm.kalandar@gmail.com>
2021-12-08 20:42:02 +0600

    added django-jazzmin

--- eduwise/settings.py
+++ eduwise/settings.py
@@ -45,6 +45,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
