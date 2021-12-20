b75fbacebec17a40fa131ace8f5bbe269699de35
mamatkasym <mm.kalandar@gmail.com>
2021-12-21 00:41:10 +0600

    changed media url

--- eduwise/settings.py
+++ eduwise/settings.py
@@ -236,7 +236,7 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 
-MEDIA_URL = 'api/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # REST_AUTH_TOKEN_MODEL = 'rest_framework_simplejwt.tokens.Token'
