f8a80933e8e61fbb2048fc75f394229459c342c2
mamatkasym <mm.kalandar@gmail.com>
2022-01-06 15:50:07 +0600

    added drop down filters

--- locations/admin.py
+++ locations/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+
+from django_admin_listfilter_dropdown.filters import RelatedDropdownFilter
+
 from .models import City, Province, Country, CountryI18N, ProvinceI18N, CityI18N
+from .forms import CountryForm
 
 
 class CityI18NInlineAdmin(admin.TabularInline):
@@ -8,6 +12,12 @@ class CityI18NInlineAdmin(admin.TabularInline):
 
 class CityAdmin(admin.ModelAdmin):
     inlines = [CityI18NInlineAdmin]
+    list_display = ("name", 'province', 'country')
+    search_fields = ("name", 'province__name', 'country__name')
+    list_filter = (
+        ('province', RelatedDropdownFilter),
+        ('country', RelatedDropdownFilter),
+    )
 
     class Meta:
         model = City
@@ -15,14 +25,21 @@ class CityAdmin(admin.ModelAdmin):
 
 class CityInlineAdmin(admin.TabularInline):
     model = City
+    extra = 0
 
 
 class ProvinceI18NInlineAdmin(admin.TabularInline):
     model = ProvinceI18N
+    extra = 0
 
 
 class ProvinceAdmin(admin.ModelAdmin):
     inlines = [ProvinceI18NInlineAdmin, CityInlineAdmin]
+    list_display = ('name', 'country')
+    search_fields = ("name", )
+    list_filter = (
+        ('country', RelatedDropdownFilter),
+    )
 
     class Meta:
         model = Province
@@ -30,17 +47,19 @@ class ProvinceAdmin(admin.ModelAdmin):
 
 class ProvinceInlineAdmin(admin.TabularInline):
     model = Province
+    extra = 0
 
 
 class CountryI18NInlineAdmin(admin.TabularInline):
     model = CountryI18N
+    extra = 0
 
 
 class CountryAdmin(admin.ModelAdmin):
     inlines = [CountryI18NInlineAdmin, ProvinceInlineAdmin]
-
-    class Meta:
-        model = Country
+    form = CountryForm
+    list_display = ['name', 'iso_code']
+    search_fields = ("name", )
 
 
 admin.site.register(Country, CountryAdmin)
