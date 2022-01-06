4403cb99d1043da5e078083a999a8cbdc41e055d
mamatkasym <mm.kalandar@gmail.com>
2022-01-06 15:53:18 +0600

    added institution admin

--- institutions/admin.py
+++ institutions/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Institution, InstitutionType
+
+
+class InstitutionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'get_province', 'get_country')
+    raw_id_fields = ("city",)
+    search_fields = ('name', )
+
+    class Meta:
+        model = Institution
+
+    @admin.display(ordering='city__province', description='Province')
+    def get_province(self, obj):
+        return obj.city.province
+
+    @admin.display(ordering='city__country', description='Country')
+    def get_country(self, obj):
+        return obj.city.country
+
+
+admin.site.register(Institution, InstitutionAdmin)
+admin.site.register(InstitutionType)
