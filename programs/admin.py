5f67fc7e462a9a76c2d2a068018a1ff7af0af80f
mamatkasym <mm.kalandar@gmail.com>
2022-01-07 20:15:56 +0600

    added programs admin

--- programs/admin.py
+++ programs/admin.py
@@ -1,4 +1,26 @@
 from django.contrib import admin
-from .models import Program
+from .models import Program, Fee, Degree
 
-admin.site.register(Program)
+
+class FeeInlineAdmin(admin.TabularInline):
+    model = Fee
+
+
+class ProgramAdmin(admin.ModelAdmin):
+    inlines = [FeeInlineAdmin]
+    fieldsets = (
+        (None, {
+           'fields': ('title', 'website', 'duration', 'institution', 'degree')
+        }),
+        ('Dates', {
+            'fields': (('start', 'end'), 'deadline' ),
+        }),
+    )
+    list_display = ('title', 'institution')
+
+    class Meta:
+        model = Program
+
+
+admin.site.register(Degree)
+admin.site.register(Program, ProgramAdmin)
