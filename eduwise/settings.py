b78bc63916bddf0f177631db0a883cea008bac41
mamatkasym <mm.kalandar@gmail.com>
2022-01-07 20:20:23 +0600

    added smart select and filter

--- eduwise/settings.py
+++ eduwise/settings.py
@@ -46,7 +46,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
+    # 'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'drf_yasg',
     'corsheaders',
 
+    'smart_selects',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -72,6 +73,8 @@ INSTALLED_APPS = [
     'ckeditor',
     'ckeditor_uploader',
     'adminsortable2',
+    'django_filters',
+    'django_admin_listfilter_dropdown',
 
     # Eduwise apps
     'users.apps.UsersConfig',
@@ -129,7 +132,8 @@ DATABASES = {
         'USER': env('POSTGRES_USER', default='polygon'),
         'HOST': env('POSTGRES_HOST', default='localhost'),
         'PORT': env('POSTGRES_PORT', default='5432'),
-        'PASSWORD': env('POSTGRES_PASSWORD', default='AC1298861')
+        'PASSWORD': env('POSTGRES_PASSWORD', default='AC1298861'),
+        'ATOMIC_REQUESTS': True
     }
 }
 
@@ -163,19 +167,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-PARLER_LANGUAGES = {
-    # SITE_ID=1
-    1: (
-        {'code': 'en', },
-        {'code': 'it', },
-        {'code': 'nl', },
-    ),
-    'default': {
-        'fallback': 'en',  # defaults to PARLER_DEFAULT_LANGUAGE_CODE
-        'hide_untranslated': False,  # the default; let .active_translations() return fallbacks too.
-    }
-}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
@@ -270,89 +261,37 @@ CKEDITOR_BASEPATH = "/api/static/ckeditor/ckeditor/"
 CKEDITOR_UPLOAD_PATH = "uploads/"
 
 JAZZMIN_SETTINGS = {
-    # title of the window (Will default to current_admin_site.site_title if absent or None)
     "site_title": "Eduwise Admin",
-
-    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
     "site_header": "EDUWISE",
-
-    # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
     "site_brand": "Library",
-
-    # Logo to use for your site, must be present in static files, used for brand on top left
-    # "site_logo": "books/img/logo.png",
-
-    # CSS classes that are applied to the logo above
     "site_logo_classes": "img-circle",
-
-    # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
     "site_icon": None,
-
-    # Welcome text on the login screen
     "welcome_sign": "Welcome to the library",
-
-    # Copyright on the footer
     "copyright": "Acme Library Ltd",
-
-    # The model admin to search from the search bar, search bar omitted if excluded
     "search_model": "users.User",
-
-    # Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
     "user_avatar": None,
-
-    ############
-    # Top Menu #
-    ############
-
-    # Links to put along the top menu
     "topmenu_links": [
-
-        # Url that gets reversed (Permissions can be added)
         {"name": "Home", "url": "admin:index", "permissions": ["auth.view_user"]},
-
-        # external url that opens in a new window (Permissions can be added)
-        # {"name": "Support", "url": "https://github.com/farridav/django-jazzmin/issues", "new_window": True},
-
-        # model admin to link to (Permissions checked against model)
         {"model": "auth.User"},
-
-        # App with dropdown menu to all its models pages (Permissions checked against models)
         {"app": "contents"},
         {"app": "locations"},
         {"app": "programs"},
         {"app": "users"},
     ],
-
-    #############
-    # User Menu #
-    #############
-
-    # Additional links to include in the user menu on the top right ("app" url type is not allowed)
     "usermenu_links": [
         {"name": "Support", "url": "https://github.com/farridav/django-jazzmin/issues", "new_window": True},
         {"model": "auth.user"}
     ],
-
-    #############
-    # Side Menu #
-    #############
-
-    # Whether to display the side menu
     "show_sidebar": True,
 
-    # Whether to aut expand the menu
     "navigation_expanded": True,
 
-    # Hide these apps when generating side menu e.g (auth)
     "hide_apps": [],
 
-    # Hide these models when generating side menu (e.g auth.user)
     "hide_models": [],
 
-    # List of apps (and/or models) to base side menu ordering off of (does not need to contain all apps/models)
     "order_with_respect_to": ["contents", "programs", "locations", "users"],
 
-    # Custom links to append to app groups, keyed on app name
     "custom_links": {
         "books": [{
             "name": "Make Messages",
@@ -362,44 +301,22 @@ JAZZMIN_SETTINGS = {
         }]
     },
 
-    # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
-    # for the full list of 5.13.0 free icon classes
     "icons": {
         "auth": "fas fa-users-cog",
         "auth.user": "fas fa-user",
         "auth.Group": "fas fa-users",
     },
-    # Icons that are used when one is not manually specified
+
     "default_icon_parents": "fas fa-chevron-circle-right",
     "default_icon_children": "fas fa-circle",
 
-    #################
-    # Related Modal #
-    #################
-    # Use modals instead of popups
     "related_modal_active": False,
 
-    #############
-    # UI Tweaks #
-    #############
-    # Relative paths to custom CSS/JS scripts (must be present in static files)
     "custom_css": None,
     "custom_js": None,
-    # Whether to show the UI customizer on the sidebar
     "show_ui_builder": False,
 
-    ###############
-    # Change view #
-    ###############
-    # Render out the change view as a single form, or in tabs, current options are
-    # - single
-    # - horizontal_tabs (default)
-    # - vertical_tabs
-    # - collapsible
-    # - carousel
     "changeform_format": "horizontal_tabs",
-    # override change forms on a per modeladmin basis
     "changeform_format_overrides": {"auth.user": "collapsible", "auth.group": "vertical_tabs"},
-    # Add a language dropdown into the admin
-    # "language_chooser": True,
 }
+
