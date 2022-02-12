6181a54e513e1a21229530e4b16b72900b94a023
mamatkasym <mm.kalandar@gmail.com>
2022-02-12 16:17:31 +0600

    added price range filter

--- institutions/filters.py
+++ institutions/filters.py
@@ -1,9 +1,20 @@
+from django.db.models import F
 from django_filters import rest_framework as filters
 from .models import Institution
 
 
 class InstitutionFilter(filters.FilterSet):
+    price_min = filters.NumberFilter(method='min_price')
+    price_max = filters.NumberFilter(method='max_price')
 
     class Meta:
         model = Institution
         fields = ['city', 'institution_type']
+
+    @staticmethod
+    def min_price(queryset, name, value):
+        return queryset.annotate(total=F('fee__housing') + F('fee__tuition') + F('fee__other')).filter(total__gte=value)
+
+    @staticmethod
+    def max_price(queryset, name, value):
+        return queryset.annotate(total=F('fee__housing') + F('fee__tuition') + F('fee__other')).filter(total__lte=value)
